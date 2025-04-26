7a9aab73a1c9cf2fec4896c27d9baad11f70ba4a
mhyddnn <amiddin12@gmail.com>
2025-04-26 21:16:55 +0700

    update 100 rb data

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,13 +14,22 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
-        foreach (range(1, 10000) as $index) {
-            Product::create([
-                'name' => $faker->word(),  // Nama produk acak
-                'description' => $faker->sentence(),  // Deskripsi produk acak
-                'price' => $faker->randomFloat(2, 1000, 10000),  // Harga produk acak (2 decimal, antara 1000 dan 10000)
-            ]);
+        $faker = Faker::create('id_ID');  // Menetapkan locale bahasa Indonesia
+
+        // Menggunakan chunk untuk menghindari masalah memori saat menulis banyak data
+        $chunkSize = 1000;  // Menyimpan data dalam 1000 baris setiap kali
+        foreach (range(1, 100) as $index) { // 100 kali chunk, berarti 100.000 data
+            $products = [];
+            foreach (range(1, $chunkSize) as $i) {
+                $products[] = [
+                    'name' => $faker->word(),
+                    'description' => $faker->sentence(),
+                    'price' => $faker->randomFloat(2, 1000, 10000),  // Harga produk antara 1000 - 10000
+                ];
+            }
+
+            // Insert batch data
+            Product::insert($products);
         }
     }
 }
